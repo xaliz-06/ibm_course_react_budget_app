c9dbd19cfe23867634deda194ea8cda3e5fed615
xaliz-06 <vaibhavchakraborty06@gmail.com>
2024-03-06 14:02:04 -0500

    Revert "updated files from completed lab"
    This reverts commit 72a6fe896da67a184ddfdc0d7289829c045ba31c.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Code to import Budget.js
 import Budget from './components/Budget';
-import Remaining from './components/Remaining'
-import ExpenseTotal from './components/ExpenseTotal';
-import ExpenseList from './components/ExpenseList';
-import ExpenseItem from './components/ExpenseItem';
-import AllocationForm from './components/AllocationForm';
 
 // Add code to import the other components here under
 
@@ -20,33 +15,29 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-            
-                            <div className='col-sm'>
-                                <Budget />
-                            </div>       
+                        {
+                            /* Add Budget component here */
+                        }        
 
-                            <div className='col-sm'>
-                                <Remaining />
-                            </div>     
+                        {
+                            /* Add Remaining component here*/
+                        }        
 
-                            <div className='col-sm'>
-                                <ExpenseTotal />
-                            </div>   
-                        
-                        <div>
-                            <h3 className='mt-3'>Allocation</h3>
-                            <div className='col-sm'>
-                                <ExpenseList />
-                            </div>               
-          
-                            <h3 className='mt-3'>Change Allocation</h3>
-                            <div className='col-sm'>
-                                <AllocationForm />
-                            </div> 
-                        </div>
-
+                        {
+                            /* Add ExpenseTotal component here */
+                        }        
                        
-                                
+                        {
+                            /* Add ExpenseList component here */
+                        }         
+
+                        {
+                            /* Add ExpenseItem component here */
+                        }        
+
+                        {
+                            /* Add AllocationForm component here under */
+                        }        
 
                 </div>
             </div>
